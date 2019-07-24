98c7fb72909a53a1575cf6031aea816bbb06b079
Adrian Jones <Nboaram@users.noreply.github.com>
2019-07-24 10:16:45 +0100

    Capitalised a G

--- docker-composeInstall.sh
+++ docker-composeInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl https://get.docker.com | bash
-sudo usermod -ag docker $(whoami)
+sudo usermod -aG docker $(whoami)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose -$(uname -s) -$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
